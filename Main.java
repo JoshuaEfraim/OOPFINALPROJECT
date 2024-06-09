f1a14813cbf728ebe42706c97cf6a77b3c2d4eda
JoshuaEfraim <140325887+JoshuaEfraim@users.noreply.github.com>
2024-06-10 02:08:21 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -17,7 +17,9 @@ public class Main {
                 for (BigInteger result : results)
                     System.out.print(new String(result.toByteArray()));
                 System.out.println(")");
+                //Will switch the Alice and Bob name if the current is Bob means the name will be Alice and vice versa
                 System.out.println("(" + (current.equals(Bob) ? Alice.getName() : Bob.getName()) + " reads (after decoding): " + current.decrypt(results) + ")");
+                //If it is currently Bob, then it will switch to Alice
                 if (current.equals(Bob))
                     current = Alice;
                 else
